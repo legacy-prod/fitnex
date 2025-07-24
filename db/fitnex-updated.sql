61cda461bb50739ca6e59d0f4466bf3963dcea52
Asjad <asjadmmc67@gmail.com>
2025-07-25 04:01:48 +0500

    database updated

--- db/fitnex-updated.sql
+++ db/fitnex-updated.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2025 at 08:34 PM
+-- Generation Time: Jul 25, 2025 at 12:56 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
